080d6ae499731c99a6b8c1a02addba6e71182ee0
Esther Brown <estherb@fb.com>
2019-06-27 16:51:48 -0700

    Movie cell and refresher on table view

--- Flix/MoviesViewController.m
+++ Flix/MoviesViewController.m
@@ -8,12 +8,14 @@
 
 #import "MoviesViewController.h"
 #import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (nonatomic, strong) NSArray *movies;
+@property(nonatomic, strong) UIRefreshControl *refreshControl;
 @end
 
 @implementation MoviesViewController
@@ -24,7 +26,14 @@
     self.tableView.dataSource= self;
     self. tableView.delegate= self;
     
-    // Do any additional setup after loading the view.
+    [self fetchMovies];
+    
+    self.refreshControl=[[UIRefreshControl alloc] init];
+    [self. refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+    
+}
+-(void) fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -47,10 +56,10 @@
             // TODO: Store the movies in a property to use elsewhere
             // TODO: Reload your table view data
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
 }
-
 -(void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     //Dispose of any resources that can be created.
@@ -66,7 +75,13 @@
     NSDictionary *movie = self.movies[indexPath.row];
     cell.titleLabel.text= movie[@"title"];
     cell.synopsisLabel.text=movie[@"overview"];
-   // cell.textLabel.text= movie[@"title"];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    NSURL *posterURL= [NSURL URLWithString: fullPosterURLString];
+    cell.posterView.image= nil;
+    [cell.posterView setImageWithURL: posterURL];
+    // cell.textLabel.text= movie[@"title"];
     return cell;
 }
 
