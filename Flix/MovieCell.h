eefc5c74cad0a71940be9b72fe7d213189a7cb4a
Esther Brown <estherb@fb.com>
2019-07-14 17:19:56 -0700

    Cleaning code for view controllers

--- Flix/MovieCell.h
+++ Flix/MovieCell.h
@@ -15,7 +15,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UIImageView *posterView;
 @property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
 
-
 @end
 
 NS_ASSUME_NONNULL_END
